85db1aba590b651e2cd146708f58a57f64add14d
Femi Senjobi <femijohn25@gmail.com>
2019-12-16 11:20:18 +0100

    used star wars logo created a skeleton app with dummy details

--- src/App.js
+++ src/App.js
@@ -6,18 +6,57 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Choose a star wars movie
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <select>
+          <option></option>
+          <option>Movie 1</option>
+          <option>Movie 2</option>
+          <option>Movie 3</option>
+          <option>Movie 4</option>
+        </select>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>This is the introtext of the starwars movie... This is the introtext of the starwars movie... This is the introtext of the starwars movie... This is the introtext of the starwars movie... This is the introtext of the starwars movie... This is the introtext of the starwars movie...</p>
+        <table>
+          <thead>
+            <tr>
+              <td><button>Name</button></td>
+              <td><button>Gender</button></td>
+              <td><button>Height</button></td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Obi Wan Ken-Obi</td>
+              <td>Male</td>
+              <td>50</td>
+            </tr>
+            <tr>
+              <td>Obi Wan Ken-Obi</td>
+              <td>Male</td>
+              <td>50</td>
+            </tr>
+            <tr>
+              <td>Obi Wan Ken-Obi</td>
+              <td>Male</td>
+              <td>50</td>
+            </tr>
+            <tr>
+              <td>Obi Wan Ken-Obi</td>
+              <td>Male</td>
+              <td>50</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td></td>
+              <td></td>
+              <td>170cm (50ft/11iches)</td>
+            </tr>
+          </tfoot>
+
+        </table>
       </header>
     </div>
   );
